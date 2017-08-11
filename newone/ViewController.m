a689ec46c8095c21e9dbfd5c188064e490eb648c
Benjamin <874635819@qq.com>
2017-08-11 14:01:57 +0800

    yeah

--- newone/ViewController.m
+++ newone/ViewController.m
@@ -18,6 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     NSArray *arr = [NSArray new];
+    NSDictionary *dic = [NSDictionary new];
 }
 
 
